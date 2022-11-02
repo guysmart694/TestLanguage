b729e6a8cd58bf3a25f4ebbd67c90044af2d5e80
hiteshsathawane <hiteshsathawane@gmail.com>
2022-11-02 11:15:23 +0530

    add project files

---
+++ src/main/java/com/example/demo/TestLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLanguageApplication.class, args);
+	}
+
+}
